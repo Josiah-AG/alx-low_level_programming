8cd8da604ba8f9f8ac01e7567bf5333261956f13
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-09 10:52:36 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - mainfunction
+ * Return: 0
+ */
+
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'z'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
