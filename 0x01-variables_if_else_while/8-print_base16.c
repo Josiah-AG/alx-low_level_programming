64b527f692ec930628cef5d1e8c830ac28411e28
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-09 11:28:13 -0800

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,12 @@ int main(void)
 	int num;
 	char le;
 
-	for (num = '0'; num < 10; num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (le = 'a'; le <= 'f'; le++)
 		putchar(le);
+
 	putchar('\n');
 
 	return (0);
