12ba0d0ad8a100917c8622c312978f544f64e6b3
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-13 09:24:47 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,22 +3,19 @@
 /**
  * _abs - absolute value of an intiger
  * @n: the intiger
- * Return: 0
+ * Return: absolute value
  */
 
 int _abs(int n)
 {
-	int a;
-
 	if (n < 0)
 	{
-		a = -1 * n;
-		_putchar(a);
+		return (-n)
 	}
 	else
 	{
-		_putchar(n);
+		return (n);
 	}
 
-	return (0);
+
 }
