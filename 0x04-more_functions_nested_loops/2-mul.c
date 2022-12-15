4cb2d371c7e7346ea7827fe7a058611889644c44
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-15 11:06:14 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two numbers
+ * @a: number one
+ * @b: number two
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	printf("%d * %d", a, b);
+}
