39f788eaa90b7e5c5377b45a8819a7332c5e50ed
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-15 06:30:27 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - chek capial letter
+ * @c: input letter
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && <= 90)
+		return (1);
+	else
+		return (0);
+}
