66a8d135043a73176646213f866895e698e193b5
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-19 10:26:01 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - main function
+ * @str: input function
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i >= 0);
+
+	{
+		if (str[i] == '\0')
+		{
+			_putchar ('\n');
+			break;
+		}
+		if (i % 2 == 0)
+			_putchar (str[i]);
+		i++;
+	}
+}
