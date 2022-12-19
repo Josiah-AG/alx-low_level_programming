72c39e4644e0682a8d05ca07e17b5516d8d0a4cb
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-19 09:21:33 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Main function
+ * @str: input
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
