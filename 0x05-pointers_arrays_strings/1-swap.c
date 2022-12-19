dda2d086018e66d5816b867954e4683f8676e002
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-19 08:48:02 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - main function
+ * @a: first input
+ * @b: second input
+ * Return: Void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int vala;
+	int valb;
+
+	*a = vala;
+	*b = valb;
+
+	*a = valb;
+	*b = vala;
+}
