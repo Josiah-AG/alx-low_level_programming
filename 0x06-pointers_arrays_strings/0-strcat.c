824576ca312285276542c563f122f45c70244221
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-21 13:38:06 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
+	while (dest[i++])
 		j++;
-	}
+
+	for(i = 0; src[i]; i++)
+		dest[j] = dest[i];
 	return (dest);
 }
+
 
