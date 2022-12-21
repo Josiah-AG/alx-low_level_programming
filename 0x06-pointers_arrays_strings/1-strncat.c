75b12afd2814bddf5ee84912026915af87442b00
Eyosiyas <realgaz.eys@gms@gmail.com>
2022-12-21 06:00:44 -0800

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - main function
+ * @dest: first input
+ * @src: 2nd input
+ * @n: the maximum number
+ * Return: destination function
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while ((src[j] != '\0') && (j < n))
+	{
+		dest[i] = src[i];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
