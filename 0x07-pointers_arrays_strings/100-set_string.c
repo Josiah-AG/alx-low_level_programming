1c3f6c106bfa934dea1e5b093295b05a97bf0523
Eyosiyas <realgaz.eys@gms@gmail.com>
2023-01-02 14:04:06 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - main function
+ * @s: first variable
+ * @to: second variable
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
