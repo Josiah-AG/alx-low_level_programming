35800edbf4f00abc07a1874f7fd538be26559218
Eyosiyas <realgaz.eys@gms@gmail.com>
2023-01-04 09:51:21 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - main function
+ * @n: input
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+}
